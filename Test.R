b958e2177fbce80647cef8dd39f1981b1998f1f1
s3688570 <s3688570@student.rmit.edu.au>
2018-04-21 18:16:28 +1000

    Load libraries

--- Test.R
+++ Test.R
@@ -1,5 +1,5 @@
-data(cars)
-
-plot(cars)
-
-plot(cars, col="red")
+library(mlr)
+library(tidyverse) # for ggplot and data wrangly
+library(ggvis) # ggplot visualisation in shiny app
+library(rJava)
+library(FSelector)
